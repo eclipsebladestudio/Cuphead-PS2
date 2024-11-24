fdcebc518c06878a8095581a2f17232f2b2e3007
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-11-24 13:24:41 -0300

    add FlipX and FlipY, fix offsetX bug

--- src/Modules/sprite.js
+++ src/Modules/sprite.js
@@ -6,6 +6,9 @@ export class Sprite {
 
   spritesheet;
 
+  flipX = false
+  flipY = false
+
   frames = 0;
   currentFrame = 1;
 
@@ -39,7 +42,7 @@ export class Sprite {
           x: jumper.imageOffsetX + (i * jumper.widthPerImage),
           y: jumper.imageOffsetY,
 
-          offsetX: jumper.offsetY == undefined ? 0 : jumper.offsetX,
+          offsetX: jumper.offsetX == undefined ? 0 : jumper.offsetX,
           offsetY: jumper.offsetY == undefined ? 0 : jumper.offsetY,
 
           imagesLength: jumper.imagesLength,
@@ -81,8 +84,8 @@ export class Sprite {
 
     const frame = this.images[this.currentFrame - 1];
 
-    this.spritesheet.width = (frame.widthPerImage * frame.imagesLength) / frame.imagesLength;
-    this.spritesheet.height = (frame.heightPerImage * frame.imagesLength) / frame.imagesLength;
+    this.spritesheet.width = this.flipX ? -Math.abs(frame.widthPerImage) : Math.abs(frame.widthPerImage);
+    this.spritesheet.height = this.flipY ? -Math.abs(frame.heightPerImage) : Math.abs(frame.heightPerImage);
 
     this.spritesheet.startx = frame.x;
     this.spritesheet.endx = frame.x + frame.widthPerImage;
@@ -90,7 +93,10 @@ export class Sprite {
     this.spritesheet.starty = frame.y;
     this.spritesheet.endy = frame.y + frame.heightPerImage;
 
-    this.spritesheet.draw(this.x + frame.offsetX, this.y + frame.offsetY);
+    this.spritesheet.draw(
+      (this.flipX ? this.x + frame.widthPerImage : this.x + frame.offsetX) + frame.offsetX,
+      (this.flipY ? this.y + frame.heightPerImage : this.y + frame.offsetY) + frame.offsetY
+    );
 
   }
 
